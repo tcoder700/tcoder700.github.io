fefb776f3a13876fc95cf2246f8ea01b9b517ba7
tcoder700 <71659340+tcoder700@users.noreply.github.com>
2022-03-27 06:19:25 +0530

    Updated Content.js

--- Content.js
+++ Content.js
@@ -6,14 +6,16 @@ if(convertedUrl.includes("iframe")){
     var init = function(){
         const injectFrame = document.createElement('iframe');
         const injectButton = document.createElement('button');
-        const lineBreak = document.createElement('br');
+        
         injectButton.innerHTML= "Hover Button";
         injectFrame.innerHTML = urlGet;
         document.body.appendChild(injectButton);
-        document.body.appendChild(lineBreak);
+        
         document.body.appendChild(injectFrame);
         
-        alert("Inserting the iframe and the button");};
+        alert("Inserting the iframe and the button");
+        alert("the url is" + urlGet);
+    };
     init();    
 }
 else{
